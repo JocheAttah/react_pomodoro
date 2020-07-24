59b6f2e845515341278902dc0f8760fd7f34910e
Attah David <jocheattahdavid@gmail.com>
2020-07-24 22:03:26 +0100

    styled functional app

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# React Pomodoro App
+ ![Snapshot](screen.jpg)
+
+### live
+yet to post
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useRef} from 'react';
 import './App.css';
 
+
+function padTime(time){
+  return time.toString().padStart(2, '0');
+}
+
 function App() {
+  const [title, setTitle] = useState('let the countdown begin!!!');
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+  function startTimer(){
+    // console.log(intervalRef.current);
+    if (intervalRef.current !== null) return;
+
+    setTitle(`You're doing great!`);
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        if(timeLeft >= 1) return timeLeft -1;
+        // resetTimer();
+        return 0;
+      });
+    }, 1000);
+  }
+
+
+  function stopTimer() {
+    // console.log(intervalRef.current);
+    if(intervalRef.current === null) return;
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle('Keep it up!');
+    setIsRunning(false);
+  }
+  
+
+  function resetTimer(){
+    // console.log(intervalRef.current);
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle('Ready to go for another round?');
+    setTimeLeft(25 * 60);
+    setIsRunning(false);
+  }
+
+
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{title}</h2>
+      
+      <div className='timer'>
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+      
+      <div className='buttons'>
+        { !isRunning && <button onClick={startTimer} >Start</button>}
+        { isRunning && <button onClick={stopTimer} >Stop</button>}
+        <button onClick={resetTimer} >Reset</button>
+      </div>
+      
     </div>
   );
 }
